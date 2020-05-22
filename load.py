a649c395ccf4140329a01e66ef8666292787a430
omnivore13 <bnb14mails@gmail.com>
2020-05-22 11:13:42 +1000

    Minor tweaks to fix output dataframe

--- load.py
+++ load.py
@@ -2,32 +2,28 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-import json
 
-client_secret = input('Please enter your Client Secret Key: (WARNING: DO NOT SHARE THIS KEY WITH ANYONE!): ')
-client_id = input("Please Enter your Client ID here: ")
-user_id = input('Please Enter your User ID Here: ')
-playlist_id = input('Please Enter Playlist ID to get audio features for all tracks in playlist: ')
+
+client_secret = str(input('Please enter your Client Secret Key: (WARNING: DO NOT SHARE THIS KEY WITH ANYONE!): '))
+client_id = str(input("Please Enter your Client ID here: "))
+user_id = str(input('Please Enter your User ID Here: '))
+playlist_id = str(input('Please Enter Playlist ID to get audio features for all tracks in playlist: '))
 def chunks(l, n):
     for i in range(0, len(l), n):
         yield list(l[i:i + n])
 
 
-secret = """Your Client Secret Key Here"""
-cid = """Your Client ID Here"""
-client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id,
+                                                      client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 sp.trace = False
-tracks = sp.user_playlist(user=user_id,playlist_id=playlist_id)
+tracks = sp.user_playlist(user=user_id, playlist_id=playlist_id)
 songs = tracks['tracks']['items']
 ids = [song['track']['id'] for song in songs]
 track_details = []
 mytracks = chunks(ids, 50)
 for chunk in mytracks:
-    track_chunk = (sp.tracks(tracks=ids[:50]))
+    track_chunk = (sp.tracks(tracks=list(chunk)))
     for track in track_chunk['tracks']:
         track_details.append([track['id'], track['name'], track['artists'][0]['name']])
 features = sp.audio_features(ids)
@@ -36,5 +32,5 @@ td = pd.DataFrame(track_details)
 td.rename(columns={0: 'id', 1: 'Track Name', 2: 'Lead Artist'}, inplace=True)
 df = (pd.merge(features, td))
 df.index = df['id']
-df.drop(['uri','track_href','analysis_url','id', 'time_signature'], axis=1, inplace=True)
+df.drop(['uri', 'track_href', 'analysis_url', 'time_signature', 'id', 'type'], axis=1, inplace=True)
 df.to_csv('mydata.csv')
